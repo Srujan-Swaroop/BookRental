194b3f1d730164d2c2f50ba3e1a92db65ce1a095
Srujan-Swaroop <swaroop.srujan@gmail.com>
2018-11-28 10:57:24 +0530

    first

---
+++ Book Rentals/products.php
@@ -0,0 +1,200 @@
+<?php
+
+//if (session_status() !== PHP_SESSION_ACTIVE) {session_start();}
+if(session_id() == '' || !isset($_SESSION)){session_start();}
+include 'config.php';
+?>
+
+<!doctype html>
+<html class="no-js" lang="en">
+
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Books </title>
+    <link rel="stylesheet" href="css/foundation.css" />
+    <script src="js/vendor/modernizr.js"></script>
+    <style >
+    body{
+        background-image:url("images/blur-book-shelf-landscape.png");
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        background-position: top; 
+        background-size: 1920px 1080px;
+      }
+      .sidenav {
+  height: 100%;
+  width: 200px;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  background-color: #111;
+  overflow-x: hidden;
+  padding-top: 20px;
+}
+
+/* Style the sidenav links and the dropdown button */
+.sidenav a, .dropdown-btn {
+  padding: 6px 8px 6px 16px;
+  text-decoration: none;
+  font-size: 20px;
+  color: #818181;
+  display: block;
+  border: none;
+  background: none;
+  width:100%;
+  text-align: left;
+  cursor: pointer;
+  outline: none;
+}
+
+/* On mouse-over */
+.sidenav a:hover, .dropdown-btn:hover {
+  color: #f1f1f1;
+}
+
+/* Main content */
+.main {
+  margin-left: 200px; /* Same as the width of the sidenav */
+  font-size: 20px; /* Increased text to enable scrolling */
+  padding: 0px 10px;
+}
+
+/* Add an active class to the active dropdown button */
+.active {
+  background-color: green;
+  color: white;
+}
+
+/* Dropdown container (hidden by default). Optional: add a lighter background color and some left padding to change the design of the dropdown content */
+.dropdown-container {
+  display: none;
+  background-color: #262626;
+  padding-left: 8px;
+}
+
+/* Optional: Style the caret down icon */
+.fa-caret-down {
+  float: right;
+  padding-right: 8px;
+}
+
+   
+
+</style>
+  </head>
+  <body>
+
+    <nav class="top-bar" data-topbar role="navigation">
+      <ul class="title-area">
+        <li class="name">
+          <h1><a href="index.php">Book Rental Store</a></h1>
+        </li>
+        <li class="toggle-topbar menu-icon"><a href="#"><span></span></a></li>
+      </ul>
+
+      <section class="top-bar-section">
+      <!-- Right Nav Section -->
+        <ul class="right">
+          <li><a href="about.php">About</a></li>
+          <li class='active'><a href="products.php">Books</a></li>
+          <li><a href="cart.php">Cart</a></li>
+          <li><a href="orders.php">My Orders</a></li>
+          <li><a href="contact.php">Contact</a></li>
+          <?php
+
+          if(isset($_SESSION['username'])){
+            echo '<li><a href="account.php">My Account</a></li>';
+            echo '<li><a href="logout.php">Log Out</a></li>';
+          }
+          else{
+            echo '<li><a href="login.php">Log In</a></li>';
+            echo '<li><a href="register.php">Register</a></li>';
+          }
+          ?>
+        </ul>
+      </section>
+    </nav>
+
+
+
+
+    <div class="row" style="margin-top:20px;">
+      <div class="small-12">
+        <?php
+          $i=0;
+          $product_id = array();
+          $product_quantity = array();
+
+          $result = $mysqli->query('SELECT * FROM books');
+          if($result === FALSE){
+            die(mysql_error());
+          }
+
+          if($result){
+
+            while($obj = $result->fetch_object()) {
+              echo '<div style="background-color:#CCDADA; margin-top:30px;" class="columns">';
+              echo '<p ><h3 style="color: #000000;text-align:center;"><b>'.$obj->title.'</h3></b></p>';
+              echo '<img src="images/products/'.$obj->image.'". width=250px height=250px align="left" hspace="20" />';
+              echo '<p style="color: #000000;margin-top:10px;"> <strong>Author</strong>: '.$obj->author.'</p>';
+              echo '<p style="color: #000000;margin-top:10px;"> <strong>Description</strong>: <br />'.$obj->description.'</p>';
+              echo '<p style="color: #000000"><strong>Price</strong> : '.$obj->price.'</p>';
+              echo '<p style="color: #000000"><strong>Category</strong> : '.$obj->category.'</p>';
+
+              // <form>
+              //    <input type="text" id="number" value="0"/>
+              //    <input type="button" onclick="incrementValue()" value="Increment Value" />
+              // </form>
+               
+
+
+               echo '<p><a href="update-cart.php?action=add&id='.$obj->id.'"><input type="submit" value="Add To Cart" style="clear:both; background: #0078A0; border: none; color: #fff; font-size: 1em; padding: 10px" /></a></p>';
+
+
+              // if($obj->qty > 0){
+              //   echo '<p><a href="update-cart.php?action=add&id='.$obj->id.'"><input type="submit" value="Add To Cart" style="clear:both; background: #0078A0; border: none; color: #fff; font-size: 1em; padding: 10px;" /></a></p>';
+              // }
+              // else {
+              //   echo 'Out Of Stock!';
+              // }
+              echo '<p> <br/><br/><br/></p>';
+              echo '</div> ';
+
+              $i++;
+            }
+
+          }
+
+          $_SESSION['product_id'] = $product_id;
+
+
+          echo '</div>';
+          echo '</div>';
+          ?>
+
+        <div class="row" style="margin-top:10px;">
+          <div class="small-12">
+
+
+
+
+        <footer style="margin-top:10px;">
+           <p style="text-align:center; font-size:0.8em;color: #000000">&copy; Book Rentals pvt ltd.</p>
+        </footer>
+
+      </div>
+    </div>
+
+
+
+
+
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+  </body>
+</html>
